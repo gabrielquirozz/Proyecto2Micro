bcb78f289d7d7c9144707668a1f524688168b66a
Jose Pablo Ponce <46695351+JosePabloPonce@users.noreply.github.com>
2020-10-12 23:55:44 -0600

    Update Laboratorio4_JosePonce_GabrielQuiroz.cpp

--- Laboratorio4_JosePonce_GabrielQuiroz.cpp
+++ Laboratorio4_JosePonce_GabrielQuiroz.cpp
@@ -23,6 +23,11 @@ string bufferclave64[64];
 int bufferclave56permutacionpc1[56];
 int izquierdaclave[28];
 int derechaclave[28];
+int izquierdadesplazadoclave[28];
+int derechadesplazadoclave[28];
+int izquierdayDerechaClaveDesplazamiento2bit[56]
+int bufferclave48permutacionpc2[48];
+
 
 
 
@@ -47,6 +52,18 @@ int permutacionPC1[56] = {
          7, 62, 54, 46, 38, 30, 22,
         14,  6, 61, 53, 45, 37, 29,
         21, 13, 5 , 28, 20, 12,  4
+    };
+	
+	
+int permutacionPC2[48] = {
+        14, 17, 11, 24,  1,  5,
+         3, 28, 15,  6, 21, 10,
+        23, 19, 12,  4, 26,  8,
+        16,  7, 27, 20, 13,  2,
+        41, 52, 31, 37, 47, 55,
+        30, 40, 51, 45, 33, 48,
+        44, 49, 39, 56, 34, 53,
+        46, 42, 50, 36, 29, 32
     };
 
 //Subrutina para leer el archivo de texto y posteriormente convertir los caracteres a binario.
@@ -144,10 +161,47 @@ return NULL;
 	
 }
 
+
+void *ClaveDesplazamientoLS(NULL){	
+
+izquierdadesplazadoclave[26] = izquierdaclave[0];
+izquierdadesplazadoclave[27] = izquierdaclave[1];
+derechadesplazadoclave[26] = derechaclave[0];
+derechadesplazadoclave[27] = derechaclave[1];
+
+for(int i =0; i<26; i++){
+	
+	izquierdadesplazadoclave[i] = izquierdaclave[i+2];
+	derechadesplazadoclave[i]=  derechaclave[i+2]
+}
+
+for (int k=0; k<28; k++){
+
+	izquierdayDerechaClaveDesplazamiento2bit[k] = izquierdadesplazadoclave[k];
+	izquierdayDerechaClaveDesplazamiento2bit[28+k] = derechadesplazadoclave[k];
+	
+}
+
+
+return NULL;
+
+
+}
+
+void *PermutacionPC2F(NULL){	
+
+	for(int i=0; i<48; i++){
+		bufferclave48permutacionpc2[i]= izquierdayDerechaClaveDesplazamiento2bit[permutacionPC2[i]-1]);
+	}
+	
+
+}
+
+
 int main(){
 
 	pthread_t thread, thread2;
-	pthread_t threadC1, threadC2;
+	pthread_t threadC1, threadC2, threadC3;
 	string numero;	
 	string texto;
 	string clave;
@@ -180,4 +234,10 @@ int main(){
 	pthread_create(&threadC2,NULL,PermutacionPC1F, NULL);
 	pthread_join(threadC2, NULL);
 	
+	pthread_create(&threadC3,NULL,ClaveDesplazamientoLS, NULL);
+	pthread_join(threadC3, NULL);
+	
+	
+	
 	}
+
